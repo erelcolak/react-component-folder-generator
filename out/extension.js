e5d66915a739b46cdee6b9ae34922c289bde89d0
Erel Çolak <erelcolak@gmail.com>
2026-04-06 11:32:32 +0300

    Add files via upload

--- out/extension.js
+++ out/extension.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.activate = activate;
 exports.deactivate = deactivate;
 const vscode = require("vscode");
+const utils_1 = require("./utils");
 function activate(context) {
     let disposable = vscode.commands.registerCommand("reactComponentGenerator.generate", async (uri) => {
         let targetFolder;
@@ -17,8 +18,6 @@ function activate(context) {
         if (!componentName)
             return;
         try {
-            // camelCase conversion
-            const camelCaseName = componentName.charAt(0).toLowerCase() + componentName.slice(1);
             const componentFolderUri = vscode.Uri.joinPath(targetFolder, componentName);
             // Create folder
             await vscode.workspace.fs.createDirectory(componentFolderUri);
@@ -28,63 +27,10 @@ function activate(context) {
             const typesUri = vscode.Uri.joinPath(componentFolderUri, `${componentName}.types.ts`);
             const scssUri = vscode.Uri.joinPath(componentFolderUri, `${componentName}.module.scss`);
             // file contents
-            const contentIndexTS = `
-        export { default as ${componentName} } from "./${componentName}";\n
-      `;
-            const contentComponentTSX = `
-        import { ${componentName}Props } from "./${componentName}.types";
-        import styles from "./${componentName}.module.scss";
-
-        // ${componentName} component
-        const ${componentName} = (props: ${componentName}Props) => {
-          // destructuring props
-          const {
-            // base props
-            // variant props
-            // modifier props
-            // other props
-          } = props;
-
-          // other variables/functions/handlers
-
-          // render
-          return <div className={styles.${camelCaseName}}></div>;
-        };
-
-        // export
-        export default ${componentName};
-      `;
-            const contentComponentTypesTS = `
-        /**
-         * @author erelcolak
-         * @interface ${componentName}Props
-         * @description Prop definitions for ${componentName} component
-         */
-        export interface ${componentName}Props {
-          // element props
-          // variant props
-          // modifier props
-          // other props
-        }
-      `;
-            const contentComponentModuleSCSS = `
-        @use "variables" as *;
-        @use "mixins" as *;
-
-        // Elements
-        .${camelCaseName} {
-        }
-
-        // Variants
-        // alignVariant
-        // colorVariant
-        // styleVariant
-        // sizeVariant
-
-        // Modifiers
-
-        // Interactions
-      `;
+            const contentIndexTS = (0, utils_1.contentIndex)(componentName);
+            const contentComponentTSX = (0, utils_1.contentComponent)(componentName);
+            const contentComponentTypesTS = (0, utils_1.contentType)(componentName);
+            const contentComponentModuleSCSS = (0, utils_1.contentStyle)(componentName);
             // write file
             await vscode.workspace.fs.writeFile(indexUri, Buffer.from(contentIndexTS, "utf8"));
             await vscode.workspace.fs.writeFile(tsxUri, Buffer.from(contentComponentTSX, "utf8"));
---
+++ out/utils.js
@@ -0,0 +1,68 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.contentStyle = exports.contentType = exports.contentComponent = exports.contentIndex = exports.camelCaseName = void 0;
+const camelCaseName = (componentName) => componentName.charAt(0).toLowerCase() + componentName.slice(1);
+exports.camelCaseName = camelCaseName;
+const contentIndex = (componentName) => {
+    return `export { default as ${componentName} } from "./${componentName}";\n`;
+};
+exports.contentIndex = contentIndex;
+const contentComponent = (componentName) => {
+    return `import { ${componentName}Props } from "./${componentName}.types";
+import styles from "./${componentName}.module.scss";
+
+// ${componentName} component
+const ${componentName} = (props: ${componentName}Props) => {
+  // destructuring props
+  const {
+    // base props
+    // variant props
+    // modifier props
+    // other props
+  } = props;
+
+  // other variables/functions/handlers
+
+  // render
+  return <div className={styles.${(0, exports.camelCaseName)(componentName)}}></div>;
+};
+
+// export
+export default ${componentName};`;
+};
+exports.contentComponent = contentComponent;
+const contentType = (componentName) => {
+    return `/**
+ * @author erelcolak
+ * @interface ${componentName}Props
+ * @description Prop definitions for ${componentName} component
+ */
+export interface ${componentName}Props {
+  // element props
+  // variant props
+  // modifier props
+  // other props
+}`;
+};
+exports.contentType = contentType;
+const contentStyle = (componentName) => {
+    return `@use "variables" as *;
+  @use "mixins" as *;
+
+  // Elements
+  .${(0, exports.camelCaseName)(componentName)} {
+  }
+
+  // Variants
+  // alignVariant
+  // colorVariant
+  // styleVariant
+  // sizeVariant
+
+  // Modifiers
+
+  // Interactions
+`;
+};
+exports.contentStyle = contentStyle;
+//# sourceMappingURL=utils.js.map
