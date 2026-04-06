c19755d244eb1eb38abf0d666c34cf61a806ac2a
Erel Çolak <erelcolak@gmail.com>
2026-04-06 11:31:57 +0300

    Refactor component generation to use utility functions
    Refactored component generation logic to use utility functions for content creation.

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode";
+import { contentComponent, contentIndex, contentStyle, contentType } from "./utils";
 
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand("reactComponentGenerator.generate", async (uri: vscode.Uri) => {
@@ -14,9 +15,6 @@ export function activate(context: vscode.ExtensionContext) {
     if (!componentName) return;
 
     try {
-      // camelCase conversion
-      const camelCaseName = componentName.charAt(0).toLowerCase() + componentName.slice(1);
-
       const componentFolderUri = vscode.Uri.joinPath(targetFolder, componentName);
 
       // Create folder
@@ -29,63 +27,10 @@ export function activate(context: vscode.ExtensionContext) {
       const scssUri = vscode.Uri.joinPath(componentFolderUri, `${componentName}.module.scss`);
 
       // file contents
-      const contentIndexTS = `
-        export { default as ${componentName} } from "./${componentName}";\n
-      `;
-      const contentComponentTSX = `
-        import { ${componentName}Props } from "./${componentName}.types";
-        import styles from "./${componentName}.module.scss";
-
-        // ${componentName} component
-        const ${componentName} = (props: ${componentName}Props) => {
-          // destructuring props
-          const {
-            // base props
-            // variant props
-            // modifier props
-            // other props
-          } = props;
-
-          // other variables/functions/handlers
-
-          // render
-          return <div className={styles.${camelCaseName}}></div>;
-        };
-
-        // export
-        export default ${componentName};
-      `;
-      const contentComponentTypesTS = `
-        /**
-         * @author erelcolak
-         * @interface ${componentName}Props
-         * @description Prop definitions for ${componentName} component
-         */
-        export interface ${componentName}Props {
-          // element props
-          // variant props
-          // modifier props
-          // other props
-        }
-      `;
-      const contentComponentModuleSCSS = `
-        @use "variables" as *;
-        @use "mixins" as *;
-
-        // Elements
-        .${camelCaseName} {
-        }
-
-        // Variants
-        // alignVariant
-        // colorVariant
-        // styleVariant
-        // sizeVariant
-
-        // Modifiers
-
-        // Interactions
-      `;
+      const contentIndexTS = contentIndex(componentName);
+      const contentComponentTSX = contentComponent(componentName);
+      const contentComponentTypesTS = contentType(componentName);
+      const contentComponentModuleSCSS = contentStyle(componentName);
       // write file
       await vscode.workspace.fs.writeFile(indexUri, Buffer.from(contentIndexTS, "utf8"));
       await vscode.workspace.fs.writeFile(tsxUri, Buffer.from(contentComponentTSX, "utf8"));
